5ef208a2af5ee216a476457116e6698a29f70593
TabishVision <tabish@truemedit.com>
2023-03-13 18:13:24 +0500

    initial push

---
+++ pallets/pallet-access/src/lib.rs
@@ -0,0 +1,160 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+/// Edit this file to define custom logic or remove it if it is not needed.
+/// Learn more about FRAME and the core library of Substrate FRAME pallets:
+/// <https://docs.substrate.io/reference/frame-pallets/>
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+
+	#[pallet::pallet]
+	pub struct Pallet<T>(_);
+
+	/// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+	}
+
+	#[pallet::storage]
+	type Roles<T: Config> = StorageMap<_, Blake2_128Concat, [u8; 32], ()>;
+
+	#[pallet::storage]
+	type MemberRoles<T: Config> =
+		StorageDoubleMap<_, Blake2_128Concat, [u8; 32], Blake2_128Concat, T::AccountId, bool>;
+
+	#[pallet::genesis_config]
+	pub struct GenesisConfig {
+		pub roles: Vec<[u8; 32]>,
+	}
+
+	#[cfg(feature = "std")]
+	impl Default for GenesisConfig {
+		fn default() -> Self {
+			Self { roles: Vec::new() }
+		}
+	}
+
+	// The build of genesis for the pallet.
+	#[pallet::genesis_build]
+	impl<T: Config> GenesisBuild<T> for GenesisConfig {
+		fn build(&self) {
+			for role in &self.roles {
+				Roles::<T>::insert(role, ());
+			}
+		}
+	}
+
+	// Pallets use events to inform users when important changes are made.
+	// https://docs.substrate.io/main-docs/build/events-errors/
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		RoleAssigned { user: T::AccountId, role: [u8; 32] },
+		RoleRevoked { user: T::AccountId, role: [u8; 32] },
+	}
+
+	// Errors inform users that something went wrong.
+	#[pallet::error]
+	pub enum Error<T> {
+		AccessDenied,
+		AlreadyHasRole,
+		InvalidRole,
+		NotAssigned,
+	}
+
+	impl<T: Config> Pallet<T> {
+		// Generates and returns the unique_id and color
+		fn assign_role(user: T::AccountId, new_role: [u8; 32]) -> Result<(), DispatchError> {
+			ensure!(Roles::<T>::contains_key(&new_role), Error::<T>::InvalidRole);
+
+			if MemberRoles::<T>::contains_key(&new_role, &user) {
+				ensure!(
+					!MemberRoles::<T>::get(&new_role, &user).unwrap(),
+					Error::<T>::AlreadyHasRole
+				);
+			}
+
+			MemberRoles::<T>::insert(new_role, user.clone(), true);
+
+			Self::deposit_event(Event::RoleRevoked { user: user.clone(), role: new_role });
+
+			Ok(())
+		}
+
+		fn revoke_role(user: T::AccountId, new_role: [u8; 32]) -> Result<(), DispatchError> {
+			ensure!(Roles::<T>::contains_key(&new_role), Error::<T>::InvalidRole);
+
+			ensure!(MemberRoles::<T>::contains_key(&new_role, &user), Error::<T>::NotAssigned);
+
+			ensure!(MemberRoles::<T>::get(&new_role, &user).unwrap(), Error::<T>::NotAssigned);
+
+			MemberRoles::<T>::insert(&new_role, &user, false);
+
+			Self::deposit_event(Event::RoleAssigned { user: user.clone(), role: new_role });
+
+			Ok(())
+		}
+
+		fn validate_role(user: T::AccountId, new_role: [u8; 32]) -> Result<(), DispatchError> {
+			ensure!(Roles::<T>::contains_key(&new_role), Error::<T>::InvalidRole);
+
+			ensure!(MemberRoles::<T>::contains_key(&new_role, &user), Error::<T>::AccessDenied);
+
+			ensure!(MemberRoles::<T>::get(&new_role, &user).unwrap(), Error::<T>::AccessDenied);
+
+			Ok(())
+		}
+	}
+
+	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
+	// These functions materialize as "extrinsics", which are often compared to transactions.
+	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::weight(0)]
+		#[pallet::call_index(1)]
+		pub fn assign(
+			origin: OriginFor<T>,
+			user: T::AccountId,
+			new_role: [u8; 32],
+		) -> DispatchResult {
+			ensure_root(origin)?;
+
+			Self::assign_role(user.clone(), new_role)?;
+
+			Ok(())
+		}
+
+		#[pallet::weight(0)]
+		#[pallet::call_index(2)]
+		pub fn revoke(
+			origin: OriginFor<T>,
+			user: T::AccountId,
+			new_role: [u8; 32],
+		) -> DispatchResult {
+			ensure_root(origin)?;
+
+			Self::revoke_role(user.clone(), new_role)?;
+
+			Ok(())
+		}
+
+		#[pallet::weight(0)]
+		#[pallet::call_index(3)]
+		pub fn validate(
+			origin: OriginFor<T>,
+			user: T::AccountId,
+			new_role: [u8; 32],
+		) -> DispatchResult {
+			let _sender = ensure_signed(origin.clone())?;
+
+			Self::validate_role(user.clone(), new_role)?;
+
+			Ok(())
+		}
+	}
+}
---
+++ pallets/pallet-patient/src/lib.rs
@@ -0,0 +1,300 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::{inherent::Vec, pallet_prelude::*, traits::Randomness};
+	use frame_system::pallet_prelude::*;
+
+	pub use pallet_access;
+
+	#[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+	#[scale_info(skip_type_params(T))]
+	pub struct Patients<T: Config> {
+		pub name: BoundedVec<u8, T::MaxNameLength>,
+		pub email: BoundedVec<u8, T::MaxEmailLength>,
+		pub data_hash: Option<BoundedVec<u8, T::MaxHashLength>>,
+	}
+
+	impl<T: Config> Default for Patients<T> {
+		fn default() -> Self {
+			Patients {
+				name: BoundedVec::<u8, T::MaxNameLength>::default(),
+				email: BoundedVec::<u8, T::MaxEmailLength>::default(),
+				data_hash: None,
+			}
+		}
+	}
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config + pallet_access::Config {
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
+		/// The type of Randomness we want to specify for this pallet.
+		type RequestRandomness: Randomness<Self::Hash, Self::BlockNumber>;
+
+		#[pallet::constant]
+		type MaxNameLength: Get<u32>;
+		#[pallet::constant]
+		type MaxEmailLength: Get<u32>;
+		#[pallet::constant]
+		type MaxHashLength: Get<u32>;
+		#[pallet::constant]
+		type MaxRequestList: Get<u32>;
+	}
+
+	#[pallet::storage]
+	#[pallet::getter(fn patient_data)]
+	pub type DataMap<T: Config> = StorageMap<_, Twox64Concat, T::AccountId, Patients<T>, OptionQuery>;
+
+	#[pallet::storage]
+	pub type RequestMap<T: Config> = StorageDoubleMap<
+		_,
+		Blake2_128Concat,
+		T::AccountId,
+		Blake2_128Concat,
+		T::AccountId,
+		[u8; 16],
+	>;
+
+	#[pallet::storage]
+	pub type AprovedRequestMap<T: Config> = StorageDoubleMap<
+		_,
+		Blake2_128Concat,
+		T::AccountId,
+		Blake2_128Concat,
+		T::AccountId,
+		[u8; 16],
+	>;
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// When Patient Data is Successfully registered.
+		PatientDataUpdated {
+			patient_account_id: T::AccountId,
+			name: BoundedVec<u8, T::MaxNameLength>,
+		},
+		/// When a Request is Sucessfully added to the RequestQueue
+		RequestQueued {
+			requester: T::AccountId,
+			patient_account_id: T::AccountId,
+			request_unique_id: [u8; 16],
+		},
+		/// When a Request is Successfully Approved
+		RequestApproved {
+			requester: T::AccountId,
+			patient_account_id: T::AccountId,
+			request_unique_id: [u8; 16],
+		},
+		/// When a request is successfully executed
+		RequestFulfilled {
+			requester: T::AccountId,
+			patient_account_id: T::AccountId,
+			data_hash: BoundedVec<u8, T::MaxHashLength>,
+		},
+		/// When a request is successfully executed
+		DataUpdated { requester: T::AccountId, patient_account_id: T::AccountId },
+	}
+
+	#[pallet::error]
+	pub enum Error<T> {
+		BoundsOverflow,
+		AlreadyRegistered,
+		OnlyOneRequestAllowed,
+		AlreadyRequested,
+		NoRequest,
+		UnableToUpdate,
+	}
+
+	impl<T: Config> Pallet<T> {
+		fn gen_request_id() -> ([u8; 16]) {
+			// Create randomness
+			let random = T::RequestRandomness::random(&b"unique_id"[..]).0;
+
+			// Create randomness payload. Multiple Request can be generated in the same block,
+			// retaining uniqueness.
+			let unique_payload = (
+				random,
+				frame_system::Pallet::<T>::extrinsic_index().unwrap_or_default(),
+				frame_system::Pallet::<T>::block_number(),
+			);
+
+			// Turns into a byte arrays
+			let encoded_payload = unique_payload.encode();
+			frame_support::Hashable::blake2_128(&encoded_payload)
+		}
+
+		fn register(
+			patient_account: T::AccountId,
+			name: Vec<u8>,
+			email: Vec<u8>,
+			data_hash: Option<BoundedVec<u8, T::MaxHashLength>>,
+		) -> Result<(), DispatchError> {
+			ensure!(!DataMap::<T>::contains_key(&patient_account), Error::<T>::AlreadyRegistered);
+
+			let bounded_name: BoundedVec<u8, T::MaxNameLength> =
+				name.try_into().map_err(|_| Error::<T>::BoundsOverflow)?;
+
+			let bounded_email: BoundedVec<u8, T::MaxEmailLength> =
+				email.try_into().map_err(|_| Error::<T>::BoundsOverflow)?;
+
+			let patient =
+				Patients::<T> { name: bounded_name.clone(), email: bounded_email, data_hash };
+
+			DataMap::<T>::insert(&patient_account, patient);
+
+			Self::deposit_event(Event::PatientDataUpdated {
+				patient_account_id: patient_account,
+				name: bounded_name,
+			});
+
+			Ok(())
+		}
+
+		fn request(
+			request_unique_id: [u8; 16],
+			requester: T::AccountId,
+			patient_account_id: T::AccountId,
+		) -> Result<(), DispatchError> {
+			ensure!(
+				!RequestMap::<T>::contains_key(&patient_account_id, &requester),
+				Error::<T>::OnlyOneRequestAllowed
+			);
+
+			RequestMap::<T>::insert(&patient_account_id, &requester, &request_unique_id);
+
+			Self::deposit_event(Event::RequestQueued {
+				requester,
+				patient_account_id,
+				request_unique_id,
+			});
+
+			Ok(())
+		}
+
+		fn approve(
+			patient_account_id: T::AccountId,
+			requester: T::AccountId,
+		) -> Result<(), DispatchError> {
+			ensure!(
+				RequestMap::<T>::contains_key(&patient_account_id, &requester),
+				Error::<T>::NoRequest
+			);
+
+			let request_unique_id =
+				(RequestMap::<T>::get(&patient_account_id, &requester)).unwrap();
+
+			RequestMap::<T>::remove(&patient_account_id, &requester);
+
+			AprovedRequestMap::<T>::insert(&patient_account_id, &requester, &request_unique_id);
+
+			Self::deposit_event(Event::RequestApproved {
+				requester,
+				patient_account_id,
+				request_unique_id,
+			});
+
+			Ok(())
+		}
+	}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::weight(0)]
+		#[pallet::call_index(1)]
+		pub fn register_patient(
+			origin: OriginFor<T>,
+			name: Vec<u8>,
+			email: Vec<u8>,
+			data_hash: Option<BoundedVec<u8, T::MaxHashLength>>,
+		) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+
+			Self::register(sender.clone(), name, email, data_hash)?;
+
+			Ok(())
+		}
+
+		#[pallet::weight(0)]
+		#[pallet::call_index(2)]
+		pub fn request_patient_data(
+			origin: OriginFor<T>,
+			patient_account_id: T::AccountId,
+		) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+
+			let request_unique_id = Self::gen_request_id();
+
+			Self::request(request_unique_id, sender, patient_account_id)?;
+
+			Ok(())
+		}
+
+		#[pallet::weight(0)]
+		#[pallet::call_index(3)]
+		pub fn approve_request(origin: OriginFor<T>, requester: T::AccountId) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+
+			Self::approve(sender, requester)?;
+
+			Ok(())
+		}
+
+		#[pallet::weight(0)]
+		#[pallet::call_index(4)]
+		pub fn get_patient_data(
+			origin: OriginFor<T>,
+			patient_account_id: T::AccountId,
+		) -> DispatchResult {
+			let requester = ensure_signed(origin)?;
+
+			ensure!(
+				AprovedRequestMap::<T>::contains_key(&patient_account_id, &requester),
+				Error::<T>::NoRequest
+			);
+
+			let data = DataMap::<T>::get(&patient_account_id).unwrap().data_hash.unwrap();
+			// let s: String = data.iter().map(|x| x.encode()).collect::<Vec<_>>().concat().into();
+
+			Self::deposit_event(Event::RequestFulfilled {
+				requester,
+				patient_account_id,
+				data_hash: data,
+			});
+
+			Ok(())
+		}
+
+		#[pallet::weight(0)]
+		#[pallet::call_index(5)]
+		pub fn update_patient_data(
+			origin: OriginFor<T>,
+			patient_account_id: T::AccountId,
+			data_hash: Option<BoundedVec<u8, T::MaxHashLength>>,
+		) -> DispatchResult {
+			let requester = ensure_signed(origin.clone())?;
+
+			ensure!(
+				AprovedRequestMap::<T>::contains_key(&patient_account_id, &requester),
+				Error::<T>::NoRequest
+			);
+			pallet_access::Pallet::<T>::validate(origin, requester.clone(), [0u8;32])?;
+
+			let mut patient_data = DataMap::<T>::get(&patient_account_id).unwrap_or_default();
+
+			patient_data.data_hash = data_hash;
+
+			DataMap::<T>::insert(&patient_account_id, patient_data);
+
+			Self::deposit_event(Event::DataUpdated { requester, patient_account_id });
+
+			Ok(())
+		}
+	}
+}
